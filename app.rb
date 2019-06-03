4d03d375603ab17fcf692ea19f4797cbd1a1c098
Michael Hughes <mhughes36@live.co.uk>
2019-06-03 17:19:03 +0100

    Created a user database and signup page
    Co-authored-by: hughbric <mhughes36@live.co.uk>
    Co-authored-by: tenzinball <tenzinball@icloud.com>
    Co-authored-by: olliebrownlow <olliebrownlow@gmail.com>

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra/base'
+require_relative './lib/user'
+require_relative 'lib/database_connection_setup'
 
 class MakersBNB < Sinatra::Base
   enable :sessions
 
   get '/' do
-    'Hello world!'
+    erb :index
+  end
+
+  post '/users' do
+    User.create(email: params[:email], password: [:password])
+    redirect '/spaces'
+  end
+
+  get '/spaces' do
+    "Book a Space"
   end
 end
--- lib/user.rb
+++ lib/user.rb
@@ -2,4 +2,9 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 
 class User
+  include DataMapper::Resource
+
+  property :id,          Serial
+  property :email,       String, :required => true, :unique => true
+  property :password,    String, :required => true
 end
